bd3ece901b0b2b217c48503404c7a78f8773787b
NickJuneau <njuneau@email.sc.edu>
2026-03-22 13:40:37 -0400

    created navbar; added navbar to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,22 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import React from "react";
+import Navbar from "@/components/navbar";
 
 export const metadata: Metadata = {
-    title: "Future KTP Web App",
-    description: "KTP SP26"
+  title: "Future KTP Web App",
+  description: "KTP SP26",
 };
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-    return (
-        <html lang="en">
-        <body>
-        { children }
-        </body>
-        </html>
-    );
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body className="min-h-screen overflow-x-hidden bg-[#f8f6f1]">
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
 }
