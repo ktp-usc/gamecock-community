1678df844ed76cf571c9702c501a8b110edf6709
CMGonzalez02 <cmg16@email.sc.edu>
2026-03-31 16:50:30 -0400

    -fixed the styling of code. using tailwindcss now.

--- components/header.tsx
+++ components/header.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Header() {
     return (
-        <header style = {styles.header}>
-            <div style={styles.left}>
+        <header className = "bg-white text-[#9a251d] flex items-center justify-between px-5 py-2.5">
+            <div className="flex items-center">
                 <Image
                     src="/CommunityShop.png"
                     alt = "Gamecock CommUnity Shop Logo"
@@ -12,40 +12,14 @@ export default function Header() {
                 />
             </div>
 
-            <div style={styles.center}>
-                <h1 style={styles.title}>Gamecock CommUnity Shop</h1>
-                <p style={styles.location}>
+            <div className="flex-1 text-center">
+                <h1 className="m-0 text-2xl font-bold">
+                    Gamecock CommUnity Shop
+                </h1>
+                <p className="m-0 text-sm">
                     4000 Suite, Caroline Coliseum
                 </p>
             </div>
         </header>
     );
-}
-
-const styles: {[key: string]: React.CSSProperties} = {
-    header: {
-        backgroundColor: "white",
-        color: "#9a251d ",
-        display: "flex",
-        alignItems: "center",
-        padding: "10 px 20px",
-        justifyContent: "Space-between",
-    },
-    left: {
-        display: "flex",
-        alignItems: "center",
-    },
-    center: {
-        textAlign: "center" as const,
-        flex: 1,
-    },
-    title: {
-        margin: 0,
-        fontSize: "1.5rem",
-        fontWeight: "bold",
-    },
-    location: {
-        margin: 0,
-        fontSize: "0.9rem",
-    },
 }
