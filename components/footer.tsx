d728bcfb93c78e262ac0932d132bd322046582d0
Braden Guliano <72895782+bguliano@users.noreply.github.com>
2026-04-01 21:58:20 -0400

    KAN-6 fix spelling in footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className= "w-full border-t bg-white px-6 py-4 text-center text-sm text-gray-600">
-            <p>Gamecok Community Shop</p>
+            <p>Gamecock Community Shop</p>
             <p>Location: 701 Assembly St, Columbia, SC 29210</p>
             <p>The community shop is a way to help all students financially by providing food, clothing, and more.</p>
         </footer>
